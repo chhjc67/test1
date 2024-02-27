dca14144b0bf2e633a42309b725af2c2f9d18fc4
Juan Carlos Chaves Hernandez <chhjc@hotmail.com>
2024-02-26 21:37:32 -0500

    Cargar

--- src/app/dashboard/pages/change-detection/change-detection.component.ts
+++ src/app/dashboard/pages/change-detection/change-detection.component.ts
@@ -1,12 +1,48 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  signal,
+} from '@angular/core';
+import { TitleComponent } from '@shared/title/title.component';
 
 @Component({
   selector: 'app-change-detection',
   standalone: true,
-  imports: [CommonModule],
-  template: `<p>change-detection works!</p>`,
+  imports: [CommonModule, TitleComponent],
+  template: `
+    <app-title [title]="currentFramework()" />
+    <pre>{{ frameworkAsSignal() | json }}</pre>
+    <pre>{{ frameworkAsProperty | json }}</pre>
+  `,
   styles: ``,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class ChangeDetectionComponent {}
+export default class ChangeDetectionComponent {
+  public currentFramework = computed(
+    () => `Change detection - ${this.frameworkAsSignal().name}`
+  );
+
+  /* angular esta menos pendiente de los cambios con Signal */
+  public frameworkAsSignal = signal({
+    name: 'Angular',
+    releaseDate: 2016,
+  });
+  /* propiedad tradicional */
+  public frameworkAsProperty = {
+    name: 'Angular',
+    releaseDate: 2016,
+  };
+
+  constructor() {
+    setTimeout(() => {
+      this.frameworkAsSignal.update((value) => ({
+        ...value,
+        name: 'Asp net core',
+      }));
+      this.frameworkAsProperty.name = 'React';
+      console.log('Procesado...');
+    }, 3000);
+  }
+}
